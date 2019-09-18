00a4021f4bac984521ff17ae54d85fd4d951fe5e
2019JTM2207 <jtm192207@iitd.ac.in>
2019-09-18 09:24:31 +0530

    Progress after 30 mins.

--- ps1.py
+++ ps1.py
@@ -1,1 +1,20 @@
+#!/usr/bin/python3
 
+# Python code to get parity. 
+  
+# Function to get parity of number n.
+  
+# It returns 1 if n has odd parity,
+  
+# and returns 0 if n has even parity 
+def getParity( n ): 
+	parity = 0
+	while n: 
+		parity = ~parity 
+		n = n & (n - 1) 
+		return parity 
+  
+#program to test getParity() 
+print("Enter binary bit data that has to be transmitted.")
+n = int(input())
+print ("Parity of no ", n," = ", ( "odd" if getParity(n) else "even")) 
