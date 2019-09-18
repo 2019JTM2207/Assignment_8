b10e243b3a83621352733e7c1edee4a98188efb6
2019JTM2207 <jtm192207@iitd.ac.in>
2019-09-18 10:45:55 +0530

    progress after two hours

--- ps1.py
+++ ps1.py
@@ -1,20 +1,47 @@
 #!/usr/bin/python3
 
-# Python code to get parity. 
-  
-# Function to get parity of number n.
-  
-# It returns 1 if n has odd parity,
-  
-# and returns 0 if n has even parity 
-def getParity( n ): 
-	parity = 0
-	while n: 
-		parity = ~parity 
-		n = n & (n - 1) 
-		return parity 
-  
-#program to test getParity() 
-print("Enter binary bit data that has to be transmitted.")
-n = int(input())
-print ("Parity of no ", n," = ", ( "odd" if getParity(n) else "even")) 
+#Python code to get parity
+
+n = (input("Enter binary bit data that has to be transmitted= "))
+y = int(n)
+
+#Total number of digits in the input data
+count = 0
+while y!=0:
+	y = round(y/10)
+	count = count+1
+print("Total bits in the data ", count)
+
+#Convert string to list
+
+digits = [int(d) for d in str(n)]
+print (digits)
+
+c1 = 0
+c2 = 0
+
+for i in digits:
+	if i == 1:
+		c1 = c1+1
+	else:
+		if c1/2 == 0:
+			digits.append('1')
+			print(digits)
+		else:
+			print(digits)
+			
+word = n
+print(split(word)) 
+
+#The string 0101 is used as the bit string or flag to indicate the end of the frame.
+m = "0101"
+o = n+m
+print(o)
+x = "010" in n
+print(x)
+z = n.index("010")
+print(z)
+
+
+
+
