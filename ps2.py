aad2ddeebf4ed8080bd9a9e6978eeddef42a04cd
2019JTM2207 <jtm192207@iitd.ac.in>
2019-09-18 14:55:10 +0530

    submission

--- ps2.py
+++ ps2.py
@@ -6,7 +6,7 @@ print("Welcome to the Game!")
 player = 0
 
 count = 0
-while count < 1:
+while count < 9:
 
 	print("Enter the position : ",end="")
 	position = int(input())
@@ -17,6 +17,7 @@ while count < 1:
 	if position >=1 and position <=9:
 		if number >=1 and number <=9:	
 			print("Both position and number are valid")
+			board[int(position)] = int(number)
 		else:
 			print("Number is invalid")
 	else:
@@ -44,11 +45,34 @@ while count < 1:
 	DrawBoard()  
 
 	# Function for Checks position is empty or not    
-	def CheckPosition(x):    
-		if(board[x] == ' '):    
-			return True    
-		else:    
-			return False    
+	def CheckPosition(board):
+		for i in range(0, 8):
+			if(board[i] >=1 and board[i] <=9):    
+				return True    
+			else:    
+				return False  
+	# function for checking sum = 15
+	def checksum():
+		if (board[1]+board[2],board[3] == 15):
+			return 1
+		elif (board[4]+board[5],board[6] == 15):
+			return 1
+		elif (board[9]+board[7],board[8] == 15):
+			return 1
+		elif (board[1]+board[4],board[7] == 15):
+			return 1
+		elif (board[2]+board[5],board[8] == 15):
+			return 1
+		elif (board[3]+board[6],board[9] == 15):
+			return 1
+		elif (board[1]+board[5],board[9] == 15):
+			return 1
+		elif (board[3]+board[5],board[7] == 15):
+			return 1
+		else:
+			return 0
+
+			
 
 	print ('The count is:', count)
 	count = count + 1
