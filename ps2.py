7b099fe00f0660b2468ea2a8f77ec9ee7340383e
2019JTM2207 <jtm192207@iitd.ac.in>
2019-09-18 14:26:56 +0530

    second program constraint applied

--- ps2.py
+++ ps2.py
@@ -1,43 +1,55 @@
 #!/usr/bin/python3
 
+board = [' ',' ',' ',' ',' ',' ',' ',' ',' ',' '] 
+
 print("Welcome to the Game!")
-c = 0
-if c == 0:
-	print("Player 1's chance")
-	c = c+1
-else:
-	print("Player 2's chance")
-	c= c-1
+player = 0
 
-print("Enter the position : ",end="")
-position = int(input())
-print("Enter the number : ",end="")
-number = int(input())
+count = 0
+while count < 1:
 
-#check for constraints
-if position >=1 and position <=9:
-	if number >=1 and number <=9:	
-		print("Both position and number are valid")
+	print("Enter the position : ",end="")
+	position = int(input())
+	print("Enter the number : ",end="")
+	number = int(input())
+
+	#check for constraints
+	if position >=1 and position <=9:
+		if number >=1 and number <=9:	
+			print("Both position and number are valid")
+		else:
+			print("Number is invalid")
 	else:
-		print("Number is invalid")
-else:
-	print("Position is invalid")
+		print("Position is invalid")
 
+	#check for whether it is Player 1’s or Player 2’s chance.
 
-#draw 3X3 Numeric Tic-Tac-Toe
-def drawboard(board):
-    print ('    |   |   ')
-    print ('' +board[6]+ '    | ' +board[7]+ '  |   ' +board[8] )
-    print ('    |   |   ')
-    print ('--------------')
-    print ('    |   |   ')
-    print ('' +board[3]+ '    | ' +board[4]+ '  | ' +board[5] )
-    print ('    |   |   ')
-    print('--------------')
-    print ('    |   |   ')
-    print ('' +board[0]+ '    | ' +board[1]+ '  |' +board[2] )
-    print ('    |   |   ')
+	if player == 0:
+		print("Player 1's chance")
+		player = player+1
+	else:
+		print("Player 2's chance")
+		player = player-1
 
-drawboard(['', '', '', '', '', '', '', '', ''])
+	 
+	# Function for Draws Game Board    
+	def DrawBoard():    
+	    print(" %c | %c | %c " % (board[1],board[2],board[3]))    
+	    print("___|___|___")    
+	    print(" %c | %c | %c " % (board[4],board[5],board[6]))    
+	    print("___|___|___")    
+	    print(" %c | %c | %c " % (board[7],board[8],board[9]))    
+	    print("   |   |   ")  
 
+	DrawBoard()  
+
+	# Function for Checks position is empty or not    
+	def CheckPosition(x):    
+		if(board[x] == ' '):    
+			return True    
+		else:    
+			return False    
+
+	print ('The count is:', count)
+	count = count + 1
 
